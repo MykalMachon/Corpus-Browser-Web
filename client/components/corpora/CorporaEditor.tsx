14dd996e442257c5f4be8bb9adc35f0c4f2d230f
Mykal Machon <mykalmachon@gmail.com>
2021-01-05 10:30:53 -0800

    feat(client): abstract file interaction
    remove interaction from component, moved it into @utils/fileystem

--- client/components/corpora/CorporaEditor.tsx
+++ client/components/corpora/CorporaEditor.tsx
@@ -1,21 +1,25 @@
+import { getCorporaFolder } from '@utils/filesystem';
+import { useState } from 'react';
+
 const CorporaEditor = ({ corpora }) => {
+  const [corporaFiles, setCorporaFiles] = useState([]);
+
   const selectFolder = async () => {
-    try {
-      const [fileHandle] = await window.showOpenFilePicker();
-      console.log(fileHandle);
-    } catch (err) {
-      console.error('failed to select file');
-    }
+    const files = await getCorporaFolder();
+    setCorporaFiles(files);
   };
 
   return (
     <main>
-      <p>You selected {corpora.name}</p>
-      <button onClick={selectFolder}>Select the utterances.json file</button>
-      <button onClick={selectFolder}>Select the speakers.json file</button>
-      <button onClick={selectFolder}>Select the conversations.json file</button>
-      <button onClick={selectFolder}>Select the corpus.json file</button>
-      <button onClick={selectFolder}>Select the index.json file</button>
+      <p>You selected {corpora.name} </p>
+      <button onClick={selectFolder}>Select the unzipped corpus</button>
+      <hr />
+      <p>Corpora's Files:</p>
+      <ul>
+        {corporaFiles.map((file) => {
+          return <li>{file[0]}</li>;
+        })}
+      </ul>
     </main>
   );
 };
--- client/utils/filesystem/index.ts
+++ client/utils/filesystem/index.ts
@@ -1,6 +1,27 @@
-// select the corpora folder
-export const getCorporaFolder = async () => {};
+import { FileHandle } from 'fs/promises';
 
-export const readFile = async (baseFolder, path: string) => {};
+/**
+ * will open the directory picker and allow users to select their corpora folder
+ * @returns array of [filename: string, fileHandle: fileHandle]
+ */
+export const getCorporaFolder: Array<any> | any = async () => {
+  try {
+    const folder = await window.showDirectoryPicker();
+    const fileIterable = folder.entries();
+    const fileHandles: Array<FileHandle> = [];
+    let tempFile = (await fileIterable.next()).value;
+    while (tempFile) {
+      fileHandles.push(tempFile);
+      tempFile = (await fileIterable.next()).value;
+    }
+    console.log(fileHandles);
+    return fileHandles;
+  } catch (err) {
+    console.error('failed to select file');
+    console.error(err);
+  }
+};
 
-export const writeFile = async (baseFolder, path: string, data) => {};
+export const readFile = async (fileHandle: FileHandle) => {};
+
+export const writeFile = async (fileHandle: FileHandle, data: any) => {};
