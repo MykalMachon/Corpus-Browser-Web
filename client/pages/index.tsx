336544e94615e18f1caf671183edd1064b747038
Mykal Machon <mykalmachon@gmail.com>
2021-01-05 09:06:22 -0800

    feat(client): adding react-table and isomorphic fetch
    added some packages to help with tables and data fetching

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,38 @@
-const Index = () => {
+import { useState } from 'react';
+import { GetServerSideProps } from 'next';
+import { getCorporaDownloadUrls } from '@utils/data';
+import CorporaSelector from '@components/corpora/CorporaSelector';
+import CorporaEditor from '@components/corpora/CorporaEditor';
+
+const Index = ({ corporaDatasetUrls }) => {
+  const [selectedCorpus, setSelectedCorpus] = useState<Record<
+    string,
+    string
+  > | null>(null);
+
   return (
     <>
       <h1>Corpus Browser Web</h1>
       <p>This is the corpus browser web app!</p>
+      {selectedCorpus ? (
+        <CorporaEditor corpora={selectedCorpus} />
+      ) : (
+        <CorporaSelector
+          corpora={corporaDatasetUrls}
+          setCorpus={setSelectedCorpus}
+        />
+      )}
     </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const corporaDatasetUrls = await getCorporaDownloadUrls();
+  return {
+    props: {
+      corporaDatasetUrls,
+    },
+  };
 };
 
 export default Index;
