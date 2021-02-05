ccab5c153ee93ad453eec0d5a982bf5735c686a4
Mykal Machon <mykalmachon@gmail.com>
2021-02-05 15:25:35 -0800

    fix(server): JSON now valid under ECMA standards

--- server/utils/json.py
+++ server/utils/json.py
@@ -1,7 +1,10 @@
 import json
+import simplejson
 
 
 def read_JSON(path, file_name):
     raw_content = open(f"{path}/{file_name}", "r").read()
     formatted_content = [json.loads(jline) for jline in raw_content.splitlines()]
-    return formatted_content
+    ecma_json_string = simplejson.dumps(formatted_content, ignore_nan=True)
+    ecma_json_content = [json.loads(jline) for jline in ecma_json_string.splitlines()][0]
+    return ecma_json_content
